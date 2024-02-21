b8466fd9b8b7e7d25a463b607477b626f8da694d
ludin-lee <alsdyd0331@naver.com>
2024-02-21 12:43:18 +0900

    자료형 퀴즈_1 답안

--- app/src/main/java/Main.java
+++ app/src/main/java/Main.java
@@ -1,13 +1,8 @@
 public class Main {
     public static void main(String[] args){
-        int number = 5 ;
-        String text ="Hello World";
-        System.out.println(number);
-        System.out.println(text);
-
-        final int finalNumber =10;
-        System.out.println(finalNumber);
-        number =10;
-        System.out.println(number);
+        char s = 'S';
+        int a = 10;
+        System.out.println(s);
+        System.out.println(a);
     } 
 }
